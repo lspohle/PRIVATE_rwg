eedf039436b60702256ea67b710b1bfe70ddcdb5
Lea <121381385+lspohle@users.noreply.github.com>
2023-01-01 12:02:28 +0100

    Update ft_print_words.c

--- ft_print_words.c
+++ ft_print_words.c
@@ -8,14 +8,14 @@ void	ft_print_words(char **library, int amount, int *availability, int i)
 	int	position;
 
 	position = rand() % amount;
-    switch (availability[position])
-    {
-        case 0: // Word is avaiable (not printed yet)
-            printf(CYAN"    %d) ", i);
-            printf(ESCAPE"%s", library[position]);
-            availability[position] = 1;
-            break;
-        default: // Word is unavaiable (already printed) 
-            ft_print_words(library, amount, availability, i);
-    }
+	switch (availability[position])
+	{
+		case 0: // Word is avaiable (not printed yet)
+			printf(CYAN"    %d) ", i);
+			printf(ESCAPE"%s", library[position]);
+			availability[position] = 1;
+			break;
+		default: // Word is unavaiable (already printed) 
+			ft_print_words(library, amount, availability, i);
+	}
 }
